d190a380cee15b9d2269debb6467d890a9a37e74
Hoang-Hai-200 <64273754+Hoang-Hai-200@users.noreply.github.com>
2023-06-14 10:38:03 +0700

    The first commit

--- INSTALL/install_ohmyzsh.sh
+++ INSTALL/install_ohmyzsh.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-<<comment
-yum install tmux tree git -y
-yum install highlight -y
-yum install oracle-database-preinstall-19c -y
-
-mkdir -p /u01
-chown -R oracle:oinstall /u01
-yum install zsh -y
-
-sudo dnf install epel-release -y
-sudo dnf install btop -y
-
-
-
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -21,7 +7,3 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 git clone https://github.com/hlissner/zsh-autopair ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-autopair
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-comment
-
-
-./install_ranger.sh
